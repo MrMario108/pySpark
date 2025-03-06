4d7e0ed8e1095012c808f103835ebdf95c4d07b0
MrMario108 <m.majewski108@gmail.com>
2025-03-06 14:50:28 +0100

    update db, new queries

--- airlines.py
+++ airlines.py
@@ -10,7 +10,7 @@ def initSession() -> SparkSession:
 
     return spark
 
-def loadTables(spark: SparkSession) -> DataFrame:
+def loadTables(spark: SparkSession) -> Dict:
     # 📂 Ścieżka do bazy SQLite
     database_path = "src/airlines/newTravels.sqlite"
 #    database_path = "src/s_p.sqlite"
@@ -29,20 +29,89 @@ def loadTables(spark: SparkSession) -> DataFrame:
                          .load()
            for table in table_names}
 
-    dfs["seats"].printSchema()
-    dfs["tickets"].printSchema()
-    dfs["boarding_passes"].printSchema()
-    dfs["ticket_flights"].printSchema()
-    dfs["airports_data"].printSchema()
-    dfs["flights"].printSchema()
+    return dfs
+
+def printSchemas(dfs: Dict) -> None:
+    table_df: DataFrame
+
+    for key, table_df in dfs.items():
+        print(f"Table name: {key}")
+        table_df.printSchema()
+
+def makeFullRelations(dfs: Dict):
+    """Złączenie wszystkiego w przypadku tej bazy danych w jeden DataFrame wydaje się nie logiczne.
+        Można tą bazę inaczej zaprojektować
+        Funkcja niedokończona bo nie ma takiej potrzeby"""
+
+    dfs_all_relations: DataFrame
+    aircrafts_data: DataFrame
+    flights: DataFrame
+    boarding_passes: DataFrame
+    aircrafts_data = dfs["aircrafts_data"]
+    seats = dfs["seats"]
+    flights = dfs["flights"]
+    boarding_passes = dfs["boarding_passes"]
+
+
+    dfs_all_relations = (
+        flights
+        .join(aircrafts_data, flights.aircraft_code == aircrafts_data.aircraft_code, "left")
+        .join(seats, aircrafts_data.aircraft_code == seats.aircraft_code, "left")
+        .join(boarding_passes, flights.flight_id == boarding_passes.flight_id, "left")
+    )
+    dfs_all_relations.show()
+
+    return dfs_all_relations
+
+def seatsInfo(df: DataFrame, spark: SparkSession):
+
+    df.createOrReplaceTempView("seatsAirplane")
+    # pokaż wszystko
+    spark.sql("SELECT * FROM seatsAirplane;").show()
     
-    #df_fixed.createOrReplaceTempView("seatsSQLView")
-    #spark.sql("SELECT * FROM seatsSQLView;").show()
+    # policz ile jest samolotów
+    spark.sql("SELECT COUNT(DISTINCT aircraft_code) FROM seatsAirplane;").show()
 
-    dfs["seats"].show()
+    # jakie modele samolotów występują
+    spark.sql("SELECT DISTINCT aircraft_code FROM seatsAirplane;").show()
+    
+    # pokaż który samolot ma najwięcej miejsc
+    spark.sql("SELECT aircraft_code, COUNT(aircraft_code) as seats_number FROM seatsAirplane GROUP BY aircraft_code ORDER BY seats_number;").show()
+
+    # jakiej klasy są miejsca w danym samolocie
+    spark.sql("SELECT DISTINCT fare_conditions FROM seatsAirplane WHERE aircraft_code = '773';").show()
+    spark.sql("SELECT DISTINCT fare_conditions FROM seatsAirplane WHERE aircraft_code = 'CN1';").show()
+
+    # ile jest miejsc klasy Economy
+    spark.sql("SELECT aircraft_code, COUNT(fare_conditions) FROM seatsAirplane WHERE aircraft_code = '773' and fare_conditions = 'Economy' GROUP BY aircraft_code;").show()
+    spark.sql("SELECT aircraft_code, COUNT(fare_conditions) FROM seatsAirplane WHERE aircraft_code = '773' and fare_conditions = 'Comfort' GROUP BY aircraft_code;").show()
+    spark.sql("SELECT aircraft_code, COUNT(fare_conditions) FROM seatsAirplane WHERE aircraft_code = '773' and fare_conditions = 'Business' GROUP BY aircraft_code;").show()
 
 
+    spark.sql("SELECT aircraft_code, COUNT(fare_conditions) FROM seatsAirplane WHERE aircraft_code = 'CN1' and fare_conditions = 'Economy' GROUP BY aircraft_code;").show()
+    
+
+def showAll(dfs: Dict):
+    table_df: DataFrame
+    dfs["boarding_passes"].show()
+
+    for key, table_df in dfs.items():
+        print(f"Table name: {key}")
+        table_df.show()
+    
+    
 def main():
-    loadTables(initSession())
+    spark = initSession()
+    dfs = loadTables(spark)
+    
+    # printSchemas(dfs)
+    seatsInfo(dfs["seats"], spark)
+    
+    
+    
+    # showAll(dfs)
+    # makeFullRelations(dfs)
+    
+
 
 main()
